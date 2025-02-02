a7e2ced68caaf4ee0e48333da93107a69dd67b6d
viniciusrutkows <viniciusrutkows@gmail.com>
2025-02-02 20:55:58 -0300

    exemplos

--- exemplos.js
+++ exemplos.js
@@ -190,3 +190,109 @@ function fatorial(n) {
 return n === 1 ? 1 : n * fatorial(n - 1);
 }
 */
+
+/*
+let listOfNumbers = [2, 3, 5, 7, 11];
+console.log(listOfNumbers[2]);
+// → 5
+console.log(listOfNumbers[0]);
+// → 2
+console.log(listOfNumbers[2 - 1]);
+// → 3
+*/
+
+/*
+let doh = "Doh";
+console.log(typeof doh.toUpperCase);
+// → function
+console.log(doh.toUpperCase());
+// → DOH
+*/
+
+/*
+let sequence = [1, 2, 3];
+sequence.push(4);
+sequence.push(5);
+console.log(sequence);
+// → [1, 2, 3, 4, 5]
+console.log(sequence.pop());
+// → 5
+console.log(sequence);
+// → [1, 2, 3, 4]
+*/
+
+/*
+let day1 = {
+    squirrel: false,
+    events: ["work", "touched tree", "pizza", "running"]
+    };
+    console.log(day1.squirrel);
+    // → false
+    console.log(day1.wolf);
+    // → false
+    console.log(day1.events[0]);
+    // → work
+*/
+
+/*
+let anObject = {left: 1, right: 2};
+console.log(anObject.left);
+// → 1
+delete anObject.left;
+console.log(anObject.left);
+// → undefined
+console.log("left" in anObject);
+// → false
+console.log("right" in anObject);
+// → true
+*/
+
+/*
+const person = {
+    isHuman: false,
+    printIntroduction: function () {
+        console.log(`My name is ${this.name}. Am I human? ${this.isHuman}`);
+    },
+};
+
+var eu = Object.create(person);
+
+var cRone = Object.create(person);
+
+eu.name = 'Vinicius'; // "name" is a property set on "me", but not on "person"
+eu.isHuman = true; // Inherited properties can be overwritten
+
+cRone.name = 'suiciniV';
+cRone.isHuman = false;
+
+var chaves = Object.keys(eu);
+
+console.log(chaves);
+
+console.log(cRone);
+
+cRone = Object.assign(cRone, eu);
+
+console.log(cRone);
+*/
+
+let journal = [
+    {
+        events: [
+            "work", "touched tree", "pizza","running", "television"
+        ],
+        squirrel: false
+    },
+    {
+        events: [
+            "work", "ice cream", "cauliflower","lasagna", "touched tree", "brushed teeth"
+        ],
+        squirrel: false
+    },
+    {
+        events: [
+            "weekend", "cycling", "break", "peanuts","beer"
+        ],
+        squirrel: true
+    },
+]
